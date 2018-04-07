7a2d29282d0ecd4f754c04de255cd2b899810687
mcorbridge <mikecorbridge@gmail.com>
2018-04-07 14:31:32 -0500

    first commit

---
+++ src/Cell.java
@@ -0,0 +1,22 @@
+package src.com.mcorbridge.findisland;
+
+/**
+ * Created by Mike on 4/5/2018.
+ */
+public class Cell {
+
+
+	Cell(String value, int[] coord){
+		this.value = value;
+		this.coord = coord;
+	}
+
+
+	public String value;
+
+	public int islandNum;
+
+	public Boolean isIsland = Boolean.FALSE;
+
+	public int[] coord;
+}
