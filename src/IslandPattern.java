88c25e8fceaa55ae49d4e64965ec77baa9e2f4a2
mcorbridge <mikecorbridge@gmail.com>
2018-04-15 14:46:28 -0500

    code housekeeping

--- src/IslandPattern.java
+++ src/IslandPattern.java
@@ -3,103 +3,48 @@
 import java.util.Vector;
 
 /**
- * Created by Mike on 4/3/2018.
+ * Created by Mike on 4/15/2018.
  */
 public class IslandPattern {
+
+	public static Vector<Vector<String>> buildMatrix(String matrixString, int dim){
+		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
+		String[] temp = matrixString.split("");
+		int q = 0;
+		int z = dim;
+		for(int n=0;n<dim;n++){
+			Vector<String> v = new Vector<>();
+			for(int m=q;m<z;m++){
+				String s = temp[m];
+				v.add(s);
+			}
+			matrix.add(v);
+			q += dim;
+			z += dim;
+		}
+		return matrix;
+	}
 
 	public static String fourXfour =  	"XXOO" +
 										"XXOO" +
 										"OOOO" +
 										"OOOX";
 
-	public static Vector<Vector<String>> buildFourXFour(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = fourXfour.split("");
-		int q = 0;
-		int z = 4;
-		for(int n=0;n<4;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 4;
-			z += 4;
-		}
-		return matrix;
-	}
-
 	public static String empty =	"0000" +
 									"0000" +
 									"0000" +
 									"0000";
-
-	public static Vector<Vector<String>> buildEmpty(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = empty.split("");
-		int q = 0;
-		int z = 4;
-		for(int n=0;n<4;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 4;
-			z += 4;
-		}
-		return matrix;
-	}
 
 	public static String block =  	"XXXX" +
 									"XXXX" +
 									"XXXX" +
 									"XXXX";
 
-	public static Vector<Vector<String>> buildBlock(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = block.split("");
-		int q = 0;
-		int z = 4;
-		for(int n=0;n<4;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 4;
-			z += 4;
-		}
-		return matrix;
-	}
-
 	public static String fiveXfive =  	"XOOOX" +
 										"XXOOX" +
 										"XOOOO" +
 										"OOOXO" +
 										"OOOXO";
-
-	public static Vector<Vector<String>> buildFiveXFive(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = fiveXfive.split("");
-		int q = 0;
-		int z = 5;
-		for(int n=0;n<5;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 5;
-			z += 5;
-		}
-		return matrix;
-	}
-
 
 	public static String test =		"OOOOOO" +
 									"OOOXOO" +
@@ -108,48 +53,12 @@ public class IslandPattern {
 									"OOOOOO" +
 									"OOOOOO";
 
-	public static Vector<Vector<String>> buildTest(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = test.split("");
-		int q = 0;
-		int z = 6;
-		for(int n=0;n<6;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 6;
-			z += 6;
-		}
-		return matrix;
-	}
-
 	public static String sixXsix =	"OOOOOO" +
 									"OOXXOO" +
 									"OOOXOO" +
 									"OOOOOO" +
 									"OXOOOO" +
 									"XXXXXX";
-
-	public static Vector<Vector<String>> buildSixXSix(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = sixXsix.split("");
-		int q = 0;
-		int z = 6;
-		for(int n=0;n<6;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 6;
-			z += 6;
-		}
-		return matrix;
-	}
 
 	public static String tenXten =	"OOOXXOOOOO" +
 									"OOOXXOOOOO" +
@@ -161,25 +70,6 @@ public class IslandPattern {
 									"OOOOOOOOOO" +
 									"OOOOOOOOXX" +
 									"OOOOOOOOXX";
-
-	public static Vector<Vector<String>> buildTenXTen(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = tenXten.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
-
 
 	public static String smile = 	"0000000000000000000000000" +
 									"0000000000000000000000000" +
@@ -208,25 +98,6 @@ public class IslandPattern {
 									"0000000000000000000000000" +
 									"0000000000000000000000000";
 
-
-	public static Vector<Vector<String>> buildSmile(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = smile.split("");
-		int q = 0;
-		int z = 25;
-		for(int n=0;n<25;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 25;
-			z += 25;
-		}
-		return matrix;
-	}
-
 	public static String simple = 	"0000000000" +
 									"000000X000" +
 									"000000X000" +
@@ -238,24 +109,6 @@ public class IslandPattern {
 									"0000000000" +
 									"0000000000";
 
-	public static Vector<Vector<String>> buildSimple(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = simple.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
-
 	public static String simpleTwo = 	"0000000000" +
 										"0000000000" +
 										"0XXX000000" +
@@ -266,24 +119,6 @@ public class IslandPattern {
 										"0000000000" +
 										"0000000000" +
 										"0000000000";
-
-	public static Vector<Vector<String>> buildSimpleTwo(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = simpleTwo.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
 
 	public static String simpleThree = 	"0000000000000000000" +
 										"0000XX0000000XX0000" +
@@ -304,24 +139,6 @@ public class IslandPattern {
 										"0000000000000000000" +
 										"0000000000000000000" +
 										"0000000000000000000";
-
-	public static Vector<Vector<String>> buildSimpleThree(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = simpleThree.split("");
-		int q = 0;
-		int z = 19;
-		for(int n=0;n<19;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 19;
-			z += 19;
-		}
-		return matrix;
-	}
 
 	public static String simpleSquare = 	"00XX00000000000XX000" +
 											"00XX00000000000XX000" +
@@ -344,24 +161,6 @@ public class IslandPattern {
 											"0000000XX000000000XX" +
 											"000000000000000000XX";
 
-	public static Vector<Vector<String>> buildSimpleSquare(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = simpleSquare.split("");
-		int q = 0;
-		int z = 20;
-		for(int n=0;n<20;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 20;
-			z += 20;
-		}
-		return matrix;
-	}
-
 	public static String testU =	"OOOOOO" +
 									"OXOOXO" +
 									"OXOOXO" +
@@ -369,47 +168,11 @@ public class IslandPattern {
 									"OXXXXO" +
 									"OOOOOO";
 
-	public static Vector<Vector<String>> buildTestU(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = testU.split("");
-		int q = 0;
-		int z = 6;
-		for(int n=0;n<6;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 6;
-			z += 6;
-		}
-		return matrix;
-	}
-
 	public static String cross ="OOXOO" +
 								"OXXXO" +
 								"OOXOO" +
 								"OOOOO" +
 								"OOOOO";
-
-	public static Vector<Vector<String>> buildCross(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = cross.split("");
-		int q = 0;
-		int z = 5;
-		for(int n=0;n<5;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 5;
-			z += 5;
-		}
-		return matrix;
-	}
 
 	public static String box =	"XXXXXXXXXX" +
 								"XOOOOOOOOX" +
@@ -422,24 +185,6 @@ public class IslandPattern {
 								"XOOOOOOOOX" +
 								"XXXXXXXXXX";
 
-	public static Vector<Vector<String>> buildBox(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = box.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
-
 	public static String singleTest =	"XXXOOOOXXX" +
 										"XXOOOOOOXX" +
 										"XOOOOOOOOX" +
@@ -451,93 +196,21 @@ public class IslandPattern {
 										"XXOOOOOOOX" +
 										"XXXOOOOXXX";
 
-	public static Vector<Vector<String>> buildSingleTest(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = singleTest.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
-
 	public static String diagonal =	"XOOOO" +
 									"OXOOO" +
 									"OOXOO" +
 									"OOOXO" +
 									"OOOOX";
 
-	public static Vector<Vector<String>> buildDiagonal(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = diagonal.split("");
-		int q = 0;
-		int z = 5;
-		for(int n=0;n<5;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 5;
-			z += 5;
-		}
-		return matrix;
-	}
-
 	public static String corner =	"OOX" +
 									"OXX" +
 									"XXX";
-
-	public static Vector<Vector<String>> buildCorner(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = corner.split("");
-		int q = 0;
-		int z = 3;
-		for(int n=0;n<3;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 3;
-			z += 3;
-		}
-		return matrix;
-	}
 
 	public static String ex =	"XOOOX" +
 								"OXOXO" +
 								"OOXOO" +
 								"OXOXO" +
 								"XOOOX";
-
-	public static Vector<Vector<String>> buildEx(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = ex.split("");
-		int q = 0;
-		int z = 5;
-		for(int n=0;n<5;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 5;
-			z += 5;
-		}
-		return matrix;
-	}
 
 	public static String effu =	"0000000000" +
 								"0000000000" +
@@ -549,23 +222,5 @@ public class IslandPattern {
 								"0000000000" +
 								"0000000000" +
 								"0000000000";
-
-	public static Vector<Vector<String>> buildEffu(){
-		Vector<Vector<String>> matrix = new Vector<Vector<String>>();
-		String[] temp = effu.split("");
-		int q = 0;
-		int z = 10;
-		for(int n=0;n<10;n++){
-			Vector<String> v = new Vector<>();
-			for(int m=q;m<z;m++){
-				String s = temp[m];
-				v.add(s);
-			}
-			matrix.add(v);
-			q += 10;
-			z += 10;
-		}
-		return matrix;
-	}
 
 } // class
